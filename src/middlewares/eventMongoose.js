31f756a855121aa4a95e08c124df03091c359699
SelinLiu2023 <seline.lxy@gmail.com>
2024-12-01 11:54:06 +0100

    add message

--- src/middlewares/eventMongoose.js
+++ src/middlewares/eventMongoose.js
@@ -122,34 +122,48 @@ export const findEvent =async (req,res,next)=>{
         const id = req.params.id;
         console.log(id)
         const eventObjectId = new mongoose.Types.ObjectId(id);
-
+        console.log(updateInvitation);
         if(req.body.editByCreator === 1) next();
         const {action, guestId, isJoinIn,tasks, guestName} = req.body;
         const guestObjectId = new mongoose.Types.ObjectId(guestId);
         //no task
+        const result = await Event.updateOne({ _id: eventObjectId, "gasts._id": guestObjectId},
+        {$set: {"gasts.$.isJoinIn": isJoinIn}});
+
         if(action === 0){
-            const result = await Event.updateOne({ _id: eventObjectId, "gasts._id": guestObjectId},
-                                                {$set: {"gasts.$.isJoinIn": isJoinIn}});
+            // const result = await Event.updateOne({ _id: eventObjectId, "gasts._id": guestObjectId},
+            //                                     {$set: {"gasts.$.isJoinIn": isJoinIn}});
             req.result = result;
             res.status(200).json(req.result);
             return;
         }else if(action === 1){
-            console.log("action 1")
+            // console.log("action 1")
             let tasksCount = tasks.length;
             let updated = false;
             const event = await Event.findOne({ _id: eventObjectId});
+            const guest = event.gasts.find(gast=>gast._id.toString() === guestId);
+            guest.isJoinIn = isJoinIn;
             // console.log("event.tasks",event.tasks)
-            const taskMap = new Map(event.tasks.map(task => [task.id, task]));
-            console.log("tasks",tasks)
+            // const taskMap = new Map(event.tasks.map(task => [task.id, task]));
+            // console.log("tasks",tasks)
             for(let item of tasks){
-                // const findTask = event.tasks.find(task=> task.id == item);
-                const findTask = taskMap.get(item.id);
+                const findTask = event.tasks.find(task=> task.id == item);
+                // const findTask = taskMap.get(item);
                 // console.log("findTask", findTask)
-                if(findTask.performerCount > findTask.performers.length &&  !findTask.performers.find(performer=>performer._id === guestObjectId)){
+                if(findTask.performerCount > findTask.performers.length &&  !findTask.performers.find(performer=>performer._id.toString() === guestId)){
                     // console.log("findTask.performers",findTask.performers)
                     // findTask.performers = [...findTask.performers,
                     //     {_id: guestObjectId, userName:guestName}];
                     findTask.performers.push({_id: guestObjectId, userName:guestName});
+                    // console.log("findTask.performers",findTask.performers)
+
+                    updated = true;
+                    tasksCount--;
+                }else if(findTask.performers.find(performer=>performer._id.toString() === guestId)){
+                    // console.log("findTask.performers",findTask.performers)
+                    // findTask.performers = [...findTask.performers,
+                    //     {_id: guestObjectId, userName:guestName}];
+                    findTask.performers = findTask.performers.filter(performer=>performer._id.toString() !== guestId);
                     // console.log("findTask.performers",findTask.performers)
 
                     updated = true;
@@ -161,6 +175,7 @@ export const findEvent =async (req,res,next)=>{
             if(updated){
                 await event.save();
                 req.event = event;
+                console.log(event)
             }
             if(tasksCount === 0){
                 req.updateCompleted =true;
@@ -171,6 +186,18 @@ export const findEvent =async (req,res,next)=>{
             // console.log(req.updateCompleted,req.event)
             res.status(200).json({event:req.event,updateCompleted:req.updateCompleted});
             return;
+        }else if(action == -1){
+            const result = await Event.updateOne(
+                {_id :eventObjectId},
+                {$pull: {"tasks.$[elem].performers": {_id: guestObjectId}}},
+                {arrayFilters:[
+                    {"elem.id": {$in: tasks}}
+                ]}
+                );
+            req.result = result;
+            res.status(200).json(req.result);
+            return;
+    
         }
         // console.log(id);
         next();
